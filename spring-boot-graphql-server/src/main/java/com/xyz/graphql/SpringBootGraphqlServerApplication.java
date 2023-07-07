7ae0991caf6d9d235859fd3bd5e9791ddcc76473
zhouhongsong <hongsong.zhou@gmail.com>
2023-07-07 08:43:14 -0500

    init setup

---
+++ spring-boot-graphql-server/src/main/java/com/xyz/graphql/SpringBootGraphqlServerApplication.java
@@ -0,0 +1,13 @@
+package com.xyz.graphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGraphqlServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGraphqlServerApplication.class, args);
+	}
+
+}
---
+++ spring-boot-graphql-server/src/main/java/com/xyz/graphql/controller/BookController.java
@@ -0,0 +1,22 @@
+package com.xyz.graphql.controller;
+
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
+import org.springframework.stereotype.Controller;
+
+import com.xyz.graphql.data.Author;
+import com.xyz.graphql.data.Book;
+
+@Controller
+public class BookController {
+    @QueryMapping
+    public Book bookById(@Argument String id) {
+        return Book.getById(id);
+    }
+
+    @SchemaMapping
+    public Author author(Book book) {
+        return Author.getById(book.authorId());
+    }
+}
